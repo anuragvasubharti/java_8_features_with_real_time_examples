8104b84b045065b78a6214bdc1e4265081ecd44c
anuragvasubharti <anuragvasubharti@gmail.com>
2025-10-18 16:36:25 +0530

    Removed

--- src/java_10_miscellaneous_74/geeks1.js
+++
@@ -1,2 +0,0 @@
-// JavaScript file name with geeks1.js
-print("Welcome to Geeksforgeeks!!! Mr. "+name);
--- src/java_10_miscellaneous_74/geeks2.js
+++
@@ -1,9 +0,0 @@
-// JavaScript file name with geeks.js
-
-var func1 = function(){  
-    print("Simple JavaScript function!!!");  
-}  
-
-var func2 = function(reader){  
-    print("Hello "+reader);  
-}
